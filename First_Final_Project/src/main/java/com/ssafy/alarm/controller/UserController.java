161803c605d0eb2fe038d2a57f3d3143aba8d5a2
Jinhyunji <jinhyunji520@gmail.com>
2024-05-15 16:27:46 +0900

    UserRestController 기능 추가

--- First_Final_Project/src/main/java/com/ssafy/alarm/controller/UserController.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.alarm.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-public class UserController {
-}
--- First_Final_Project/src/main/java/com/ssafy/alarm/model/dto/User.java
+++ First_Final_Project/src/main/java/com/ssafy/alarm/model/dto/User.java
@@ -7,6 +7,39 @@ public class User {
 	private String nickname;
 	private String email;
 	
+	public User() {
+		
+	}
 	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", nickname=" + nickname + ", email=" + email
+				+ "]";
+	}
+		
 }
