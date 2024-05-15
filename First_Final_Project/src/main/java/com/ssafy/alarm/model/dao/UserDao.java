d6c08a655f1f28c060af5c25778227dc3c257666
seongyunoh <seongyunoh2@gmail.com>
2024-05-15 16:28:48 +0900

    only exerinfo & user

--- First_Final_Project/src/main/java/com/ssafy/alarm/model/dao/UserDao.java
+++ First_Final_Project/src/main/java/com/ssafy/alarm/model/dao/UserDao.java
@@ -1,5 +1,15 @@
 package com.ssafy.alarm.model.dao;
 
+import java.util.Map;
+
+import com.ssafy.alarm.model.dto.User;
+
 public interface UserDao {
 
+	public int insertUser(User user);
+	
+	public User selectOne(Map<String, String> user);
+	
+	public User selectById(String id);
+	
 }
