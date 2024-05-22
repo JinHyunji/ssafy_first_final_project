8dd8d71bdb129edfa32a4eb9b7a9411b1de760b7
seongyunoh <seongyunoh2@gmail.com>
2024-05-22 17:26:25 +0900

    마무리

---
+++ first-final-vue-project/src/components/alarm/AlarmTempList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="temp">
+        <h3>템플릿 선택하기</h3>
+        <div v-for="template in store.templates" 
+            :key="template.tempId" class="list-group ">
+            <a @click="clickTemp(template.tempId)" class="list-group-item list-group-item-action list-group-item-light">
+                {{ template.exerType }} 템플릿
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useAlarmStore } from '@/stores/alarm';
+import { useYoutubeStore } from '@/stores/youtube';
+import { onMounted, ref } from 'vue';
+const store = useAlarmStore();
+const youtubeStore = useYoutubeStore();
+
+onMounted(() => {
+  store.getTemplates();
+});
+
+const clickTemp = function(tempId) {
+    store.clickTemp(tempId);
+    youtubeStore.checkedVideoTitle = 
+}
+
+</script>
+
+<style scoped>
+    h3 {
+        margin-bottom: 30px;
+    }
+    .temp {
+        margin-left: 250px;
+        margin-right: 100px;
+        text-align: center;
+    }
+
+    .list-group {
+        margin-top: 20px;
+    }
+</style>
---
+++ first-final-vue-project/src/components/common/TheHeaderNav.vue
@@ -0,0 +1,90 @@
+<template>
+    <nav class="navbar">
+        <div class="container">
+
+            <RouterLink class="navbar-brand" :to="{name:'alarmList'}">
+
+                <div class="logos">
+                    <img src="/src/assets/img/Logo10.png" width="30px
+                    
+                    ">
+                    <h3 style="color: #FFC107;"> Alarm</h3>
+                </div>
+
+            </RouterLink>
+            <div class="nav-icons" v-if="isLogined">
+                <RouterLink to="/alarm/list">
+                    <button type="button" class="btn btn-outline-warning">나의 알람 목록</button>
+                </RouterLink>
+                <RouterLink>
+                    <button  @click="logout" class="btn btn-outline-secondary">로그아웃</button>
+                </RouterLink>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import router from '@/router';
+import { useUserStore } from "@/stores/user";
+
+const store = useUserStore();
+const isLogined = ref(false);
+
+const logoLink = isLogined === true ? "/" : "/alarm/list";
+
+const restoreSession = function() {
+    const savedUser = sessionStorage.getItem('loginUser');
+    if (savedUser) {
+        isLogined.value = true;
+    }
+}
+
+onMounted(() => {
+    restoreSession();
+})
+
+const logout = function() {
+    Swal.fire({
+        text: "로그아웃 하시겠습니까?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes"
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire({
+        text: "로그아웃 되었습니다.",
+        icon: "success"
+        });
+        store.userLogout();
+    }
+    });
+}
+
+</script>
+
+<style scoped>
+    a {
+        text-decoration: none;
+        margin-left: 20px;
+    }
+
+    .navbar {
+        border-bottom: 1px solid #e7e7e7;
+    }
+
+
+    .logos {
+        display: flex;
+        align-items: center;
+    }
+
+    h3 {
+        margin-left: 10px;
+        margin-bottom: 0px;
+    }
+
+</style>
